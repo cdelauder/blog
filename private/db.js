55a4953e4ad7fabf6e41eda640f04649914b65b0
cdelauder <cdelauder@gmail.com>
2015-03-22 10:58:35 -0700

    working on setting up the db

---
+++ private/db.js
@@ -0,0 +1,8 @@
+Posts.insert({
+  id: 1,
+  title: "Datetime Math Is Hard!",
+  content: "
+    This is example content I will actually write something on this topic very soon hopefully and whatnot.\n
+    Checking how it handles things like new lines when I do it this way.
+  "
+})
--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,12 @@
 Meteor.startup(function() {
   Meteor.isServer = true;
-  
+  Posts = new Mongo.Collection('posts');
+  Router.route('/', function () {
+    this.layout('index', {
+      data: {
+        posts: Posts.find()
+      }
+    });
+    console.log(Posts.find())
+  })
 });
